c3fbdd4a21089adf67ddeb4877c3ca6cbf78c501
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-08-01 16:35:09 +0700

    feat: loading widget and loading manager

--- lib/screens/onboarding_screen/onboarding_screen.dart
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -1,12 +1,23 @@
+import 'package:ecommerce_app/common_widgets/loading_manager.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   static const String routeName = '/onboarding-screen';
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: LoadingManager(
+        isLoading: true,
+        child: Container(child: Center(child: Text("Onboarding Screen"))),
+      ),
+    );
   }
 }
