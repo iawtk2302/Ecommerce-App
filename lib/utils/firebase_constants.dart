0a6887bf03ab7f4c9a63f0f95763b8547ca93151
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-09-08 21:18:50 +0700

    fix: conflicts with main branch

--- lib/utils/firebase_constants.dart
+++ lib/utils/firebase_constants.dart
@@ -10,15 +10,12 @@ final CollectionReference categoriesRef = firestore.collection("categories");
 final CollectionReference productsRef = firestore.collection("products");
 final CollectionReference promotionsRef = firestore.collection("promotions");
 final CollectionReference ordersRef = firestore.collection("orders");
-<<<<<<< HEAD
 final DocumentReference ordersStatisticsDocRef =
     firestore.collection("statistics").doc("orders_statistics");
 final DocumentReference productsStatisticsDocRef =
     firestore.collection("statistics").doc("products_statistics");
-=======
 final CollectionReference favoritesRef = currentUserRef.collection("favorites");
 final CollectionReference faqsRef = firestore.collection("faqs");
 final CollectionReference chatRoomsRef = firestore.collection("chat_rooms");
 final CollectionReference notificationsRef =
     firestore.collection("notifications");
->>>>>>> 8cdc948d7d5a3deff220cd93259553598ce64139
